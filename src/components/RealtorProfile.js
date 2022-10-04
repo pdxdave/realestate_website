3bff49af8bd7a7d87b9825a636b23df0d308bf1f
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-10-04 10:16:35 -0700

    changes

--- src/components/RealtorProfile.js
+++ src/components/RealtorProfile.js
@@ -4,20 +4,73 @@ import styled from 'styled-components'
 
 const RealtorProfile = () => {
   return (
-    <Wrapper>
+    <Wrapper className='page-setting'>
+      <div className="article-container">
         {team.map(t => {
             const {id, img, sold, name, quote} = t
-            return ( <article key={id}> 
-                        <p>{name}</p>
+            return ( <article key={id}>
+                      <header>
+                        <img src={img} alt={name} />
+                        <div>
+                          <p>{name}</p>
+                          <p>Sr. Realtor</p>
+                        </div>
+                      </header>
+                      
+                      <footer>
+                        <p><span>Properties sold:</span> {sold}</p>
+                        <blockquote>&#8220;{quote}&#8220;</blockquote>
+                      </footer>
+                        
                     </article> 
                 )
         })}
+      </div>
+        
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
 
+.article-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2em;
+}
+article {
+  border-radius: 6px;
+  overflow: hidden;
+  padding: 1.5em 1em;
+  background-color: #C2B6CB;
+}
+
+img {
+  height: 120px;
+  width: 120px;
+  border-radius: 50%;
+}
+
+header {
+  display: grid;
+  grid-template-columns: .75fr 1fr;
+  align-items: center;
+  
+}
+header p {
+  text-transform: capitalize;
+  margin: .15em 0;
+}
+header p:first-child{
+  font-weight: 700;
+}
+
+blockquote {
+  font-style: italic;
+}
+footer p {
+  margin-top: 1em;
+}
 
 `
 
--- src/utilities/team.js
+++ src/utilities/team.js
@@ -1,22 +1,22 @@
 export const team = [
     {
         id: 1,
-        img: '/images/oswald.jpg',
-        sold: '426',
+        img: './images/oswald.jpg',
+        sold: '315',
         name: 'sally oswald',
         quote: "My business is built on communication, dedication, and transparency. This recipe has helped me rise to the top 5% of commercial real estate agents nationwide by sales volume."
     },
     {
         id: 2,
-        img: '/images/smith.jpg',
-        sold: '215',
+        img: './images/smith.jpg',
+        sold: '236',
         name: 'jeff smith',
         quote: "As a Portland native, I have intimate knowledge of the area and a strong desire to make my home your home."
     },
     {
         id: 3,
-        img: '/images/sharp.jpg',
-        sold: '315',
+        img: './images/sharp.jpg',
+        sold: '421',
         name: 'mike sharp',
         quote: "With my sharp knowledge of the local market, I've helped over 300 families in the Lake Oswego area find their dream home — and I'm confident I can help you find yours too."
     }
